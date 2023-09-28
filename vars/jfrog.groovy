d7ce84f0f75acc8498115b88540568c2e5f28e53
bindumounica <74135079+bindumounica@users.noreply.github.com>
2023-09-28 23:47:15 +0530

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -1,4 +1,14 @@
 def call()
 {
-	sh 'python3 jfrog.py'
+	pythonScript = 'vars/jfrog.py' // Replace with the path to your Python script
+	processBuilder = new ProcessBuilder(['python3', pythonScript])
+
+	try {
+    		Process process = processBuilder.start()
+    		process.waitFor()
+    		println "Python script executed successfully."
+	} catch (IOException | InterruptedException e) {
+    		println "Error running Python script: ${e.message}"
+	}
+
 }
